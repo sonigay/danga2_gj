ab20afa761f14d72c78c739bf0c7acd138e94ed7
sonigay <43840048+sonigay@users.noreply.github.com>
2020-02-19 12:57:03 +0900

    Update test_init_final.py

--- test_init_final.py
+++ test_init_final.py
@@ -1959,18 +1959,11 @@ while True:
 				result = wks.acell('B1').value
 				tmp_sayMessage = message.content
 				sayMessage = tmp_sayMessage[len(command[4])+1:]
-
-#				embed1 = discord.Embed(
-#						title = ' :signal_strength:  ' + SearchID + ' 안내 ',
-#						description= '**```css\n' + SearchID + ' 단가는 ' + result + '```**',
-#						color=0xddffff
-#						)
 				embed2 = discord.Embed(
 						title = ' 비하인드 조회!! ',
 						description= '```' "조회자:" + message.author.display_name +"\n거래처:" + message.channel.name + ' ```',
 						color=0xddffff
 						)
-#				await client.get_channel(msg.channel.id).send(embed=embed1, tts=False)
 				await MakeSound('조회하신,' + sayMessage + '단가는' + result + '', './sound/say2')
 				await PlaySound(voice_client1, './sound/say2.wav')
 				await client.get_channel(channel).send(embed=embed2, tts=False)					
